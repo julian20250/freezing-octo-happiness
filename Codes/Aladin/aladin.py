da12b969dbe4ae32b6f010577c83bcba1d8ea9c1
julian <julianorla7@hotmail.com>
2014-12-28 21:05:02 -0500

    Para Análisis de bases de datos astronómicos.

--- Codes/Aladin/aladin.py
+++ Codes/Aladin/aladin.py
@@ -1,161 +1,181 @@
-# The code only works for computers in English or Spanish...
-
-import sys
-import os
+# It will take some time to run the script the first time...
 import numpy as np
-if sys.version_info[0] < 3:
-    from Tkinter import *
-else:
-    from tkinter import *
-import tkFileDialog 
+import matplotlib.pyplot as plt
+import sys
+from mpl_toolkits.mplot3d import Axes3D
 try:
   import odf.opendocument
-  from odf.table import *
-  from odf.text import P
 except (ImportError):
-  print "You don't have installed 'odf', to install it please visit the next link \n\n https://pypi.python.org/packages/source/o/odfpy/odfpy-0.9.6.tar.gz \n\n"
-  print ("To run the setup.py you must have (obviously) privileges, so, when you had installed the library, execute this code with the same"
-	 " permissions with which you installed the library :)")
+  print("Sorry, but I couldn't import the libraries. Maybe you don't have installed odfpy's library, to get it go to this link https://pypi.python.org/packages/source/o/odfpy/odfpy-0.9.6.tar.gz"
+    " \n\n If you have it already installed, you have to execute this script with the same permissions which it was installed.")
   sys.exit()
-# Copyright 2011 Marco Conti
-# Licensed under the Apache License, Version 2.0 (the "License"):
-
-#  	http://www.apache.org/licenses/LICENSE-2.0
-
+  
+from odf.table import *
+from odf.text import P
+#How to use the next class here http://www.marco83.com/work/173/read-an-ods-file-with-python-and-odfpy/
+#Marco Conti 2011
 class ODSReader:
-
-	# loads the file
-	def __init__(self, file):
-		self.doc = odf.opendocument.load(file)
-		self.SHEETS = {}
-		for sheet in self.doc.spreadsheet.getElementsByType(Table):
-			self.readSheet(sheet)
-	
-
-	# reads a sheet in the sheet dictionary, storing each sheet as an array (rows) of arrays (columns)
-	def readSheet(self, sheet):
-		name = sheet.getAttribute("name")
-		rows = sheet.getElementsByType(TableRow)
-		arrRows = []
-		
-		# for each row
-		for row in rows:
-			row_comment = ""
-			arrCells = []
-			cells = row.getElementsByType(TableCell)
-			
-			# for each cell
-			for cell in cells:
-				# repeated value?
-				repeat = cell.getAttribute("numbercolumnsrepeated")
-				if(not repeat):
-					repeat = 1
-					
-				ps = cell.getElementsByType(P)
-				textContent = ""
-								
-				# for each text/text:span node
-				for p in ps:
-					for n in p.childNodes:
+ 
+        # loads the file
+        def __init__(self, file):
+                self.doc = odf.opendocument.load(file)
+                self.SHEETS = {}
+                for sheet in self.doc.spreadsheet.getElementsByType(Table):
+                        self.readSheet(sheet)
+       
+ 
+        # reads a sheet in the sheet dictionary, storing each sheet as an array (rows) of arrays (columns)
+        def readSheet(self, sheet):
+                name = sheet.getAttribute("name")
+                rows = sheet.getElementsByType(TableRow)
+                arrRows = []
+               
+                # for each row
+                for row in rows:
+                        row_comment = ""
+                        arrCells = []
+                        cells = row.getElementsByType(TableCell)
+                       
+                        # for each cell
+                        for cell in cells:
+                                # repeated value?
+                                repeat = cell.getAttribute("numbercolumnsrepeated")
+                                if(not repeat):
+                                        repeat = 1
+                                       
+                                ps = cell.getElementsByType(P)
+                                textContent = ""
+                                                               
+                                # for each text/text:span node
+                                for p in ps:
+                                        for n in p.childNodes:
                                                 if (n.nodeType == 1 and n.tagName == "text:span"):
-							for c in n.childNodes:
-								if (c.nodeType == 3):
-									textContent = textContent + unicode(c.data)
-							
-						if (n.nodeType == 3):
-							textContent = textContent + unicode(n.data)
-					
-				if(textContent):
-					if(textContent[0] != "#"): # ignore comments cells
-						for rr in range(int(repeat)): # repeated?
-							arrCells.append(textContent)
-					else:
-						row_comment = row_comment + textContent + " ";
-				else:
-					for rr in range(int(repeat)):
-						arrCells.append("")
+                                                        for c in n.childNodes:
+                                                                if (c.nodeType == 3):
+                                                                        textContent = textContent + unicode(c.data)
+                                                       
+                                                if (n.nodeType == 3):
+                                                        textContent = textContent + unicode(n.data)
+                                       
+                                if(textContent):
+                                        if(textContent[0] != "#"): # ignore comments cells
+                                                for rr in range(int(repeat)): # repeated?
+                                                        arrCells.append(textContent)
+                                        else:
+                                                row_comment = row_comment + textContent + " ";
+                                else:
+                                        for rr in range(int(repeat)):
+                                                arrCells.append("")
+ 
+                        # if row contained something
+                        if(len(arrCells)):
+                                arrRows.append(arrCells)
+                               
+                        #else:
+                        #       print "Empty or commented row (", row_comment, ")"
+               
+                self.SHEETS[name] = arrRows
+               
+        # returns a sheet as an array (rows) of arrays (columns)
+        def getSheet(self, name):
+                return self.SHEETS[name]
+# End of CopyRight
 
-			# if row contained something
-			if(len(arrCells)):
-				arrRows.append(arrCells)
-				
-			#else:
-			#	print "Empty or commented row (", row_comment, ")"
-		
-		self.SHEETS[name] = arrRows
-		
-	# returns a sheet as an array (rows) of arrays (columns)
-	def getSheet(self, name):
-		return self.SHEETS[name]
-	     
-#Self
-class aladin:
-  def __init__(self):
-    pass
-  
-  def choose(self):
-    root = Tk()
-    m = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a .ods file')
-    root.destroy()
-    return m
-  
-  def interactive(self):         
-    def read():
-      def man():
-	fun = E1.get()
-	return fun
-      def extract():
-	doc = ODSReader(aladin.choose())
-	tipe = doc.getSheet(sheet)
-	sound = [] 
-	for x in tipe:
-	    for y in x:
-	      sound.append(y)
-	sound = np.array(sound)
-	sound = sound.reshape(len(tipe), len(tipe[0]))
-	return tipe, sound
-      root.destroy()
-      if os.getenv('LANG')[0] + os.getenv('LANG')[1] == "es":
-	sheet = "Hoja1"
-      elif os.getenv('LANG')[0] + os.getenv('LANG')[1] == "en":
-	sheet = "Sheet1"
-      else:
-	sheet = raw_input("Please insert this (Sheet1) but in your computer language")      
-      tipe, sound = extract()      
-      ar = []
-      for x in tipe[0]:
-	ar.append(x)
-      reet = Tk()
-      reet.wm_title("Interactive")
-      L1 = Label(reet, text="I got the data...")
-      L2 = Label(reet, text="As you might know, there are %i categories of the data that you have"%len(tipe[0]))
-      L3 = Label(reet, text ="And they are: \n")
-      E1 = Entry(reet)
-      E1.delete(0, END)
-      E1.insert(0, "Distance (Parsecs)")
-      B1= Button(reet, text="Entry", command=man)
-      L1.grid()
-      L2.grid()
-      L3.grid()
-      E1.grid(columnspan = 2, sticky=W)
-      B1.grid(row=3, sticky=E)
-      for x in tipe[0]:
-	Label(reet, text = "- "+x).grid()
-      reet.mainloop()
-    def andother():
-      root.destroy()
-      sys.exit()
-    root =Tk()
-    root.protocol('WM_DELETE_WINDOW', andother)
-    root.wm_title("Welcome!")
-    L1 = Label(root, text="Hello there! \n Here is the program I made to realize an analysis of the Simbad data got by Aladin."
-	       " I hope that the program will be interactive.")
-    B1 = Button(root, text="Let's do it", command=read)
-    B2 = Button(root, text="Exit", command=andother)
-    L1.grid(columnspan=2)
-    B1.grid(row=2, sticky=E)
-    B2.grid(row=2, column=1, sticky=W)
-    root.mainloop()
+var = ODSReader("M13.ods")
+table = var.getSheet("Hoja1")
+head = table.pop(0)
+original = {}
+for x in head:
+    original[x]=[]
 
-aladin = aladin()
-aladin.interactive()
+for x in table:
+  count = 0
+  for y in x:
+    original[head[count]].append(y)
+    count +=1
+
+# Making the data readable
+readable = dict(original)
+for x in xrange(4, 15):
+  for y in readable[head[x]]:
+    if type(y) == unicode:
+      ind = readable[head[x]].index(y)
+      readable[head[x]].remove(y)
+      readable[head[x]].insert(ind, float(y))      
+
+for x in xrange(2, 4):
+  for y in readable[head[x]]:
+      road = y.split(" ")
+      road = [float(i) for i in road]
+      cross = road[0]+road[1]/60+road[2]/3600
+      ind = readable[head[x]].index(y)
+      readable[head[x]].remove(y)
+      readable[head[x]].insert(ind, cross)
+
+plt.rc("text", usetex=True)
+# Experiments With Polar Coordinates
+# Test Side
+#fig = plt.figure()
+#ax = plt.axes(projection="3d")
+#x = np.linspace(-1, 1, 100)
+#y = (1-x**2)**.5
+#z = np.zeros(100)
+
+##Axis
+#ax.plot(np.linspace(0, 1, 100), z, z, "-g")
+#ax.plot(z, np.linspace(0, 1, 100), z, "-g")
+#ax.plot(z, z, np.linspace(0, 1, 100), "-g", label = "Axis")
+
+##Text of Axis
+#ax.text(1, 0, 0, "x")
+#ax.text(0, 1, 0, "y")
+#ax.text(0, 0, 1, "z")
+
+
+#ax.plot(x, y, z, "-r")
+#ax.plot(x, -y, z, "-r", label = "Ecliptic")
+#theta = np.linspace(0, np.pi, 100)
+#inclination = 23+5/60. #= 23 grades, 5 minutes = Earth's inclination 
+#s = inclination * np.pi / 180 # Maximun Amplitude between the mayor circles
+### ???
+#ax.plot(x, np.zeros(100), y, "-g")
+#ax.plot(x, y, z, "-b")
+#ax.plot(x, -y, -z, "-b", label = "Equator")
+#ax.plot(np.zeros(100), x, y)
+
+#ax.plot(np.zeros(2), (0,(1-s**2)**.5), (0, s))
+
+#plt.legend(fontsize=12)
+#plt.axis("off")
+#plt.show()
+# End Of Test Side
+
+#Plot the Location
+cool = list(set(readable[head[1]]))
+markers = [".", ",", "o", "v", "^", "<", ">", "1", "2", "3",
+	   "4", "8", "s", "p", "*", "h", "H", "+"]
+colors = []
+for x in xrange(len(cool)): #Generated by RGB mode
+  colors.append((np.random.uniform(0, 1), np.random.uniform(0, 1), np.random.uniform(0, 1)))
+fig = plt.figure()
+ax = fig.add_subplot(111, axisbg="black")
+ax.get_xaxis().get_major_formatter().set_scientific(False) #Removing Cientific Notation
+ax.get_xaxis().get_major_formatter().set_useOffset(False) #And exponentials
+dawn = list(cool)
+for x in xrange(len(readable[head[2]])):
+  if dawn.count(readable[head[1]][x]) == 1:
+    plt.scatter(readable[head[2]][x], readable[head[3]][x], c=colors[cool.index(readable[head[1]][x])],
+	      marker=markers[cool.index(readable[head[1]][x])], label = readable[head[1]][x])
+    dawn.remove(readable[head[1]][x])
+  else:
+    plt.scatter(readable[head[2]][x], readable[head[3]][x], c=colors[cool.index(readable[head[1]][x])],
+		marker=markers[cool.index(readable[head[1]][x])])
+plt.xlabel(r"$RA$")
+plt.ylabel(r"$\delta$", fontsize=15)
+plt.legend(bbox_to_anchor=(1, .9),
+           bbox_transform=plt.gcf().transFigure)
+ax.format_coord = lambda x,y : "RA=%g, DEC=%g"%(x, y)
+plt.show()
+
+## References
+# http://dalelane.co.uk/blog/?p=778
