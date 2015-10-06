a867b35adfd496156804a702681c9ab053ebbf67
Julián J. <julianorla7@hotmail.com>
2015-10-06 10:58:05 -0500

    Comentado.

--- Codes/Simulation/hipparcos.py
+++ Codes/Simulation/hipparcos.py
@@ -1,7 +1,10 @@
-# http://stackoverflow.com/questions/25027339/how-to-get-data-from-inspect-element-of-a-webpage-using-python
-# http://stockrt.github.io/p/emulating-a-browser-in-python-with-mechanize/
+'''
+This code was finished on October 6, 2015
+Is under GNU General Public License, see http://www.gnu.org/licenses/gpl-3.0.en.html
+By Julian Jimenez.
+'''
 from Tkinter import *
-import mechanize
+import mechanize #Probably you don't have this, you can install it here: http://wwwsearch.sourceforge.net/mechanize/download.html
 from astropy.io import ascii
 import numpy as np
 import tkFileDialog
@@ -9,9 +12,15 @@ import matplotlib.pyplot as plt
 import cookielib
 import sys
 
-flag=[]
+flag=[] #Made to pass data inside functions
 def get_data(rai):
-    rai.destroy()
+    '''
+    This function runs if you don't have a txt with the data. It opens a graphic interface similar to
+    Hipparcos Database, to introduce the parameters to search. After that, it takes the response from
+    the database and saves it as table.txt
+    '''
+    rai.destroy()#Closing the first window of the execution
+    ##Doing the interface to introduce parameters
     main=Tk()
     main.wm_title("Hipparcos")
     Label(text="Select Catalogue to search:").grid(row=0,column=0)
@@ -58,14 +67,15 @@ def get_data(rai):
     br.set_handle_robots(False)
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
+    #These are generic addheaders, does not matter if they're not the specifications of your computer
     catalogues= ["Hipparcos Main Catalogue","Tycho Main Catalogue","Tycho-2 Catalogue"]
-    br.open('http://www.rssd.esa.int/hipparcos_scripts/hipMultiSearch1.pl')
+    br.open('http://www.rssd.esa.int/hipparcos_scripts/hipMultiSearch1.pl') #Opening the webpage
     br.select_form(nr=0)
-    br.form['Catalogues'] = [catalogues[v.get()]]
+    br.form['Catalogues'] = [catalogues[v.get()]] #Obtaining all fields that can be modified
+    ##Here we write all the input we got from the graphic interface in the web page
     if b["0"].get()==1:
         br.form['ra (degrees)']=["on"]
-        br.form['ramin']=d["00"].get()
+        br.form['ramin']=d["00"].get() 
         br.form['ramax']=d["01"].get()
     if b["1"].get()==1:
         br.form['dec (degrees)']=["on"]
@@ -88,22 +98,26 @@ def get_data(rai):
         br.form['BVmin']=d["40"].get()
         br.form['BVmax']=d["41"].get()
 
-    br.submit()
-    pon=br.response().read()
-    print pon[pon.index('color="#ff0000">')+16:pon.index("request.")+8]
+    br.submit() #Sending the request
+    pon=br.response().read()#Reading the response
+    print pon[pon.index('color="#ff0000">')+16:pon.index("request.")+8]#Printing the number of values that satisfied the request
+    ##Here we obtain the table from the response and we write it in a file called table.txt
     with open("table.txt", "w") as f:
         f.write(pon[pon.index("your request.\n</font></b>")+25:pon.index("</body>\n</html>")])
         f.close()
-    flag.append("table.txt")
+    flag.append("table.txt") #Now the name of the file to be checked is table
 
 def get_file(rai):
+    '''
+    This function opens a graphic interface to search the file that is going to be analized.
+    '''
     rai.destroy()
     root = Tk()
     m = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a .txt file')
     root.destroy()
-    flag.append(m)
-
-ant = Tk()
+    flag.append(m) #Now the name of te file to be checked is m
+##Here starts the ejecution
+ant = Tk()#Simple asking window 
 ant.wm_title("Hipparcos Database")
 Label(text="Do you have a txt with the data?").grid()
 Button(ant, text="yes", command=lambda: get_file(ant)).grid(row=1, column=0)
@@ -111,6 +125,7 @@ Button(ant, text="no", command=lambda: get_data(ant)).grid(row=1, column=1)
 ant.mainloop()
    
 while True:
+    ##Here is where you introduce the combination you want
     frame = Tk()
     Label(text="Insert combination:").grid()
     Label(text="Colour Index").grid(row=1, column=0)
@@ -127,18 +142,19 @@ while True:
     Button(frame,text="Submit", command=frame.destroy).grid(row=4, column=0)
     Button(frame, text="END", command=sys.exit).grid(row=4,column=1)
     frame.mainloop()
-    data = ascii.read(flag[0])
-    par = data["col12"]
+    data = ascii.read(flag[0]) #Reading the file
+    par = data["col12"] #Reading the parallax column
     M=[]
     ind = []
-    m= data["col"+str(sec.get())]
-    for x,y,z in zip(par,m,data["col"+str(fir.get())]):
+    m= data["col"+str(sec.get())] #Reading B or V mag
+    for x,y,z in zip(par,m,data["col"+str(fir.get())]): #Obtaining B-V or V-I index
         try:
-            M.append(float(y)+5-5*np.log(1./float(x)))
-            ind.append(float(z))
+            M.append(float(y)+5-5*np.log(1./float(x))) #Obtaining the abs mag
+            ind.append(float(z)) 
         except:
             pass
-    print "There were %i valid entries"%len(M)
+    print "There were %i valid entries"%len(M) #Printing the number of valid entries
+    ##Doing the plot
     ax= plt.subplot()
     ax.set_axis_bgcolor('black')
     ax.set_ylim([max(M)+1,min(M)-1])
@@ -169,6 +185,7 @@ while True:
         if x<=.31:
             other+=1
     long = len(ind)
+    ##Degrading colors 
     col = [np.concatenate((np.array([255]*(long-count-cout)),np.linspace(255,0,cout+count)))/1000., np.concatenate((np.concatenate((np.linspace(0,255,long-other),np.array([255]*jai))),np.linspace(255,0,cout+count)))/1000. ,
            np.concatenate((np.concatenate((np.array([0]*(long-other)),np.linspace(0,255,jai))),np.array([255]*(cout+count))))/1000.]
     timer=0
@@ -180,3 +197,6 @@ while True:
         ax.scatter(x, y, s=1, color=[(col[0][timer], col[1][timer], col[2][timer])])
         timer+=1
     plt.show()
+#References:
+# http://stackoverflow.com/questions/25027339/how-to-get-data-from-inspect-element-of-a-webpage-using-python
+# http://stockrt.github.io/p/emulating-a-browser-in-python-with-mechanize/
