bb974bbd3337f9cd1ecd4158f0daa04ee0f2f4c8
Julián J. <julianorla7@hotmail.com>
2015-10-08 09:33:10 -0500

    Pasar Proxy
    Corregida brecha de seguridad y añadido https.

---
+++ Codes/Simulation/connect.sh
@@ -0,0 +1,14 @@
+#Connect to proxy
+# To prove connectivity type "curl -D- -o /dev/null -s http://www.google.com", if there is a response, there is a connection to web
+echo "Digit your username and passwd, else, type cancel in username"
+echo
+read -p "Enter Username: " a
+if [ "$a" != "cancel" ]
+then
+read -s -p "Enter Password: " b
+http_proxy="http://"$a":"$b"@proxyapp.unal.edu.co:8080/"
+export http_proxy=$http_proxy
+export https_proxy=$http_proxy
+http_proxy=''
+echo
+fi
